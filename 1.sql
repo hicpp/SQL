d61a97a2ffc67a3161a64ac8fa070a6e8259c8e8
xuegp <406266505@qq.com>
2021-11-02 16:46:16 +0800

    11

--- 1.sql
+++ 1.sql
@@ -1,3 +1,22 @@
+-- 1.4
+-- 创建数据库
+CREATE DATABASE shop;
+
+
+-- 创建表
+CREATE TABLE Product
+(
+    product_id      CHAR(4)         NOT NULL,   -- 商品编号
+    product_name    VARCHAR(100)    NOT NULL,   -- 商品名称
+    product_type    VARCHAR(32)     NOT NULL,   -- 商品种类
+    sale_price      INTEGER ,   -- 销售单价
+    purchase_price  INTEGER ,   -- 进货单价
+    regist_date     DATE,   -- 登记日期
+    PRIMARY KEY (product_id)
+);
+
+
+-- 1.5
 -- 表的删除
 DROP TABLE Product
 
